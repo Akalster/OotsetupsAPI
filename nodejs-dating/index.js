b1d02d839bcd150ff885268cdfdac1e2c9071942
Akalster <72736983+Akalster@users.noreply.github.com>
2021-12-08 03:42:37 +0100

    Replaced a few things

--- nodejs-dating/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-// port can be set by heroku
-const port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log(`now listening on port ${port}`);
-});
